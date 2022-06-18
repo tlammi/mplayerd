c16ed502e7ec274f7b53df4a6e06350177437cde
tlammi <tangojulietlima@gmail.com>
2022-06-18 17:52:52 +0300

    doc

--- mplayer/timer.py
+++ mplayer/timer.py
@@ -3,8 +3,19 @@ import threading
 
 
 class Timer:
+    """
+    Interval timer for firing events periodically after each interval
+    """
 
     def __init__(self, interval: float, on_event, args=None, kwargs=None):
+        """
+        Init timer
+
+        :param interval: Event interval in seconds
+        :param on_event: Callable invoked after each interval
+        :param args: Args to pass to on_event
+        :param kwargs: Kwargs to pass to on_event
+        """
         self._interval = interval
         self._on_event = on_event
         self._args = args or []
