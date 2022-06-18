fade26bfd0ba9aca41bb2779be353619e1f93d20
tlammi <tangojulietlima@gmail.com>
2022-06-18 20:55:17 +0300

    Separate logs more clearly

--- mplayerlib/log.py
+++ mplayerlib/log.py
@@ -2,6 +2,8 @@ import datetime
 import os
 import logging
 
+_FORMAT = "%d.%m %H:%M:%S"
+
 
 class _Handler(logging.Handler):
 
@@ -11,9 +13,16 @@ class _Handler(logging.Handler):
         self._w = os.path.join(outdir, log_name + ".warn")
         self._e = os.path.join(outdir, log_name + ".err")
 
+        for path in (self._l, self._w, self._e):
+            with open(path, "a") as f:
+                msg = f"Start time: {datetime.datetime.now().strftime(_FORMAT)}"
+                f.write("#" * len(msg) + "\n")
+                f.write(msg + "\n")
+                f.write("#" * len(msg) + "\n")
+
     def emit(self, record: logging.LogRecord) -> None:
         t = datetime.datetime.now()
-        datestr = t.strftime("%d.%m %H:%M:%S")
+        datestr = t.strftime(_FORMAT)
         msg = f"{datestr} >  {record.getMessage()}\n"
         if record.levelno >= logging.ERROR:
             with open(self._e, "a") as f:
