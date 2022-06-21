6701407a04160c2ce353c8961bd4c3fbee534542
tlammi <tangojulietlima@gmail.com>
2022-06-21 14:45:04 +0300

    Install mplayerd with pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+
+setup(
+    name="mplayerd",
+    version="0.0.1",
+    author="Toni Lammi",
+    packages=["mplayerlib", "mplayerlib.conf", "mplayerlib.media",
+        "mplayerlib.sched", "mplayer"],
+    scripts=["bin/mplayerd"],
+    description="Media player daemon",
+    install_requires=["python-vlc >= 3.0.0"]
+)
