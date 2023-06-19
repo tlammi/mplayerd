9b0ad22eb36cb8e72492bace967eb4a9cbbb62a9
tlammi <tangojulietlima@gmail.com>
2023-06-19 18:48:06 +0300

    support "root" option

--- mplayerlib/conf/conf.py
+++ mplayerlib/conf/conf.py
@@ -20,22 +20,26 @@ class Conf(dict):
 
     def __init__(self, d: dict = None, directory: str = None):
         super().__init__(**(d or dict()))
+        self.setdefault("playlists", dict())
+        self.setdefault("schedule", Schedule([]))
+        self.setdefault("root", ".")
         if d is None:
-            self["playlists"] = dict()
-            self["schedule"] = Schedule([])
             return
-        self.directory = os.path.realpath(directory)
+        self.directory = os.path.join(directory, self["root"])
+        self.directory = os.path.realpath(self.directory)
 
         for k, v in self["playlists"].items():
             if isinstance(v, str):
                 scheme, resource = uri.parse(v)
                 if scheme == "inc":
-                    p = os.path.join(self.directory, resource)
+                    # NOTE: incs ignore "root" key
+                    p = os.path.join(directory, resource)
+                    LOGGER.info("Loading playlist from %s", p)
                     self["playlists"][k] = Playlist.load(p)
                 else:
-                    self["playlists"][k] = Playlist(v, directory)
+                    self["playlists"][k] = Playlist(v, self.directory)
             else:
-                self["playlists"][k] = Playlist(v, directory)
+                self["playlists"][k] = Playlist(v, self.directory)
         if "schedule" in self:
             self["schedule"] = Schedule(self["schedule"])
         else:
